5c3415d098d7fa25ee1ca18f61455dd6468264d0
tiddal <tiddal96@gmail.com>
2020-08-14 18:59:08 +0100

    Updates

---
+++ precache-manifest.c2399603a14e48b6236fb08bdb49799f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63373f287a90fa4eb70ef2ff13d65d8c",
+    "url": "/roll-the-dice/index.html"
+  },
+  {
+    "revision": "1cf471d1928aa569d029",
+    "url": "/roll-the-dice/static/js/2.5f1813b2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/roll-the-dice/static/js/2.5f1813b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01a29ba2705aab50658e",
+    "url": "/roll-the-dice/static/js/main.a8a97ca6.chunk.js"
+  },
+  {
+    "revision": "4d33048791a430bc9ffe",
+    "url": "/roll-the-dice/static/js/runtime-main.5656c004.js"
+  }
+]);
